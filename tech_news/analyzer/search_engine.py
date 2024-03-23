e4e14b9cc10704630fddc0c197eac65d8ff18d46
Nycolas Padilha <763npp@gmail.com>
2024-03-23 19:31:30 -0300

    req 8

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import db
+from datetime import datetime
 
 
 # Requisito 7
@@ -15,10 +16,27 @@ def search_by_title(title):
         raise e
 
 
+def format_date(date):
+    try:
+        return datetime.strptime(date, "%Y-%m-%d").strftime("%d/%m/%Y")
+    except ValueError:
+        raise ValueError("Data inválida")
+
+
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    formated_date = format_date(date)
+
+    try:
+        query = db.news.find(
+            {"timestamp": formated_date},
+            projection=["title", "url"],
+        )
+
+        return [(item["title"], item["url"]) for item in query]
+
+    except Exception as e:
+        raise e
 
 
 # Requisito 9
